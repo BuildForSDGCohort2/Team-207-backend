62643cd06929a1f929d52cca0ee69658634b7925
alice <amusukwa@gmail.com>
2020-08-30 21:05:15 +0100

    Adding homepage

--- AfroDevMeet/app/src/main/java/com/alice/afrodevmeet/MainActivity.java
+++ AfroDevMeet/app/src/main/java/com/alice/afrodevmeet/MainActivity.java
@@ -1,9 +1,14 @@
 package com.alice.afrodevmeet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -12,13 +17,32 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    // mTextMessage.setText(R.string.title_home);
+                    return true;
+               case R.id.navigation_dashboard:
+                  // Goquestions();
+                    return true;
+                case R.id.navigation_notifications:
+                   // mTextMessage.setText(R.string.title_notifications);
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
@@ -27,4 +51,38 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (item.getItemId()){
+            case R.id.action_profile:
+                return true;
+            case R.id.action_home:
+                Toast.makeText(this,"home",Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //To go to the questions listview
+    public void Golistview(MenuItem item) {
+        Intent intent = new Intent(MainActivity.this,Listview.class);
+        startActivity(intent);
+    }
+
+    //Onclick function for the bottom navigation
+    public void GoListanswers(MenuItem item) {
+        Toast.makeText(this,"Comments",Toast.LENGTH_LONG).show();
+    }
+
+    public void Home(MenuItem item) {
+        Toast.makeText(this,"Home",Toast.LENGTH_LONG).show();
+    }
 }
